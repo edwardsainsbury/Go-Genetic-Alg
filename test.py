b229a72869d496f677968060318f49e84ad6c078
edwardsainsbury <edwardsainsbury@me.com>
2017-05-07 22:05:07 +0100

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import random
-import timeit
-
-print(timeit.timeit('random.random()',setup = 'import random', number=10000))
-
-print(timeit.timeit( 'random.SystemRandom().random()',setup ='import random',number=10000))
