c7c1096cbf08b0a3e0b880a363d73ee8cac24ebb
edwardsainsbury <edwardsainsbury@me.com>
2017-05-10 17:55:06 +0100

    Added plotting of time/match

--- graph.py
+++ graph.py
@@ -1,5 +1,5 @@
 import mysql.connector
-
+from datetime import datetime
 import plotly as py
 import plotly.graph_objs as go
 
@@ -22,14 +22,16 @@ def graph():
             id_array.update({array[0]: 0}) if array[0] not in id_array else id_array
             id_array.update({array[1]: 0}) if array[1] not in id_array else id_array
             if array[2] == 1 and array[3] == 0:
-                id_array[array[0]] += 1
+                id_array[array[0]] += 3
             elif array[2] == 0 and array[3] == 0:
+                id_array[array[1]] += 3
+            elif array[3] == 1:
                 id_array[array[1]] += 1
         era_data.append(id_array)
     # Create traces
     trace_array = []
     trace2_array = []
-    for x in range(highest_id):
+    for x in range(highest_id+1):
         values = []
         era = []
         id_lines = []
@@ -42,6 +44,22 @@ def graph():
         trace2_array.append(go.Scatter(x=era, y=id_lines, mode='lines+markers', name=str(x)))
     py.offline.plot(trace_array, filename='EraScores.html')
     py.offline.plot(trace2_array, filename='PlayerLongevity.html')
+
+    cursor.execute("SELECT timestamp FROM matches")
+    times = cursor.fetchall()
+    diff_array = []
+    match_array = []
+    trace3_array = []
+    runningAverage = []
+    sum = 0
+    for x in range(len(times)-1):
+        diff_array.append((times[x+1][0]-times[x][0]).total_seconds()/60)
+        sum += (times[x+1][0]-times[x][0]).total_seconds()/60
+        runningAverage.append(sum/(x+1))
+        match_array.append(x)
+    trace3_array.append(go.Scatter(x=match_array, y=diff_array, mode='lines+markers', name='Time'))
+    trace3_array.append(go.Scatter(x=match_array, y=runningAverage, mode='lines+markers', name='Average'))
+    py.offline.plot(trace3_array, filename='MatchTime.html')
     cnx.close()
 
 graph()
